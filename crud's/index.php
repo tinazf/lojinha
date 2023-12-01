d9ac3a7665e4115a3814ab5922997cfe7a655b29
tinazf <Valentina.2021318295@aluno.iffar.edu.br>
2023-12-01 10:25:44 -0300

    commit2
    Nesse commit arrumei a pasta onde estavam os arquivos, chatgpt desenvolveu a tela index e um formulário

---
+++ crud's/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lojinha de Decoração</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1 class="mb-4">Lojinha de Decoração</h1>
+
+        <!-- Botões para CRUD de Produtos -->
+        <div class="mb-4">
+            <a href="readpro.php" class="btn btn-primary">Listar Produtos</a>
+            <a href="createpro.php" class="btn btn-success">Inserir Produto</a>
+        </div>
+
+        <!-- Botões para CRUD de Categorias -->
+        <div class="mb-4">
+            <a href="readcat.php" class="btn btn-primary">Listar Categorias</a>
+            <a href="createcat.php" class="btn btn-success">Inserir Categoria</a>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
